6818b4db5e51a7a982c2a809a7c1ec857fa66c79
sadigzadeamina07 <saddigzadeamina7@gmail.com>
2025-11-03 03:16:12 +0400

    some changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
-let  marka=document.getElementById('marka')
-let carts=document.getElementById('carts')
-data.map(item=>
+let marka = document.getElementById('marka');
+let carts = document.getElementById('carts');
+/*data.map(item=>
 {
     marka.innerHTML+=`<option value="${item.brand}">${item.brand}</option>`
 }
-)
+) */
+
+[...new Set(data.map(i => i.brand))].forEach(a => {
+    marka.innerHTML += `<option value="${a}">${a}</option>`;
+});
+
+function showcars(oturulendeyer) {
+    carts.innerHTML = ''
+    oturulendeyer.map(item => {
+        carts.innerHTML += `
+    <div class="cart">
+    <img src="${item.images}" alt="">
+    <div>
+    <h3>${item.price} AZN</h3>
+    <h4>${item.brand} ${item.model}</h4>
+    <h4>${item.year} ${item.engine}${item.odometer}</h4>
+    <p>${item.dates}</p>
+    </div>
+    </div>
+    `
+    }
+    )
+}
+
+showcars(data)
